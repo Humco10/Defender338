92a49459b28000d3984b4786a93f4e7bd158b467
bpj299 <38541524+bpj299@users.noreply.github.com>
2018-04-21 10:27:23 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 7474
+
+s.connect((host, port))
+print('Connected to', host)
+
+while True:
+    z = input("Enter something for the server: ")
+    s.send(z.encode())
+    
+    print('[Waiting for response...]')
+    print (s.recv(1024).decode())
