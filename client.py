db4480677d14baa4574f7beb887fdcf190714dcb
bpj299 <38541524+bpj299@users.noreply.github.com>
2018-04-25 11:30:05 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python
 #basic client
 import socket
+import select
+import sys
+import msvcrt
 
-s = socket.socket()
-host = socket.gethostname()
-port = 7474
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print("Incorrect usage: Please enter in the following format. \nIP, port")
+    exit()
+    
+ip = str(sys.argv[1])
+port = int(sys.argv[2])
 
-s.connect((host, port))
-print('Connected to', host)
+s.connect((ip, port))
 
 while True:
-    z = input("Enter something for the server: ")
-    s.send(z.encode())
-    
-    print('[Waiting for response...]')
-    print (s.recv(1024).decode())
+    socket_list = [s]
+
+    read_sockets = select.select([s], [] , [], 1)[0]
+    if msvcrt.kbhit():
+        read_sockets.append(sys.stdin)
+
+    for socks in read_sockets+[sys.stdin]:
+        if socks == s:
+            message = socks.recv(2048).decode()
+            print(message)
+            break
+        else:
+            message = input('Enter Message: ')
+            s.send(message.encode())
+            sys.stdout.write("You: ")
+            sys.stdout.write(message + "\n")
+            sys.stdout.flush()
+s.close()
