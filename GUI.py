16799ca21c1d26fdd0f01883908bab26e30534d1
Humberto Colin <31320435+Humco10@users.noreply.github.com>
2018-04-26 16:12:16 -0500

    a few changes text box instead of label

--- GUI.py
+++ GUI.py
@@ -4,27 +4,33 @@ from tkinter import *
 class Convert(Frame):
     """Sets up window and widgets"""
     def __init__(self):
-        Frame.__init__(self)
-        self.canvas=Canvas(self, width=200,height=200, bg ="white")
-        self.canvas.grid(row=0, column=0)
         
+        Frame.__init__(self)
         self.master.title("Client")
-        Frame(self.master,width=200,height=100)
+        Frame(self.master,width=300,height=300)
         self.grid()
-        self.grid(sticky= N+S+E+W)
+
+        self._tVar = StringVar()
+        self.canvas=Text(self, height = 20, width = 50)
+        self.canvas.grid(row=0, column=0,columnspan=1)
         
         #Label Field for textBox
         self._fLabel=Label(self,)
         self._fLabel.grid(row=1, column=0)
-        self._fVar=DoubleVar()
-        self._fVar.set("")
-        self._fEntry=Entry(self, textvariable=self._fVar, justify="center")
+        self._sVar=StringVar()
+        self._sVar.set("")
+        self._fEntry=Entry(self, textvariable=self._sVar, justify="center")
         self._fEntry.grid(row=2, column=0)
 
         #Command button 1
-        self._button=Button(self, text="Send")
+        self._button=Button(self, text="Send", command = self.add_one)
         self._button.grid(row=3, column=0,columnspan=2)
         #self._button.pack(side=BOTTOM, anchor=S)
+
+    def add_one(self):
+        s = self._sVar.get()
+        s += '\n'
+        self.canvas.insert(END,s)
 def main():
     """Instantiate and pop up the window"""
     Convert().mainloop()
